6eecdbebfac0740ed8f4edb2764f75dfe98ac31b
Jesper Wendel Devantier <j@pseudonymous.me>
2016-12-17 21:57:46 +0100

    adjust font for tpad

--- ze-emacs/.emacs.d/modules/ze-theme.el
+++ ze-emacs/.emacs.d/modules/ze-theme.el
@@ -9,7 +9,7 @@
   ;; 1) M-x 'set-default-font'  -- TAB twice to get a list
   ;; 2) scroll through, try each until you find something you like
   ;; 3) M-x 'describe-font' to read back the selection, copy this name
-  (set-default-font "Source Code Pro Semibold-10")
+  (set-default-font "Source Code Pro Semibold-8")
 
   (require 'powerline)
   (powerline-default-theme))
