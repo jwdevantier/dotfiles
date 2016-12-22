caf01c7f887f3914b8bceda896b832824dcf7a85
Jesper Devantier <jwd@skm.dk>
2016-12-22 13:53:34 +0100

    mac: FN=>hyper key + mini guide for hyper config

--- ze-emacs/.emacs.d/modules/ze-mac.el
+++ ze-emacs/.emacs.d/modules/ze-mac.el
@@ -8,11 +8,18 @@
   (global-set-key (kbd "<end>") 'end-of-line)
   
   ;; Swap Option (Alt) & CMD (Meta)
-
+  ;; & set 'fn' key to act as hyper
+  ;; (To get caps=>hyper:
+  ;;  1) In karabiner elements (install, then reboot)
+  ;;     - In "simple modifications", From(caps_lock), To(fn)
+  ;;  2) System Preferences, Keyboard:
+  ;;     - In "shortcuts", clear all for every section
+  ;;     - In "keyboard" tab, "Modifier keys..." btn: "Caps Lock"=>No Action, for all KB's
   (setq mac-option-key-is-meta t
 	mac-command-key-is-meta nil
 	mac-command-modifier 'meta
-	mac-option-modifier nil)
+	mac-option-modifier nil
+	mac-function-modifier 'hyper)
 
   ;; Two last settings above + this enables {} and []
   (setq x-select-enable-clipboard t))
