e447153781de8f42c281070a8c8866051cb5d6b4
Jesper Wendel Devantier <j@pseudonymous.me>
2017-03-25 22:43:57 +0100

    emacs: clojure support overhaul ("hyperspec" support + binds fix)
    * Use ze:bind for mode-specific keybinds (fixed, were inert before)
    * Add clojure "hyperspec"-like functionality. "H-h h" to lookup
      function/macro/symbol @ point or marked.

--- ze-emacs/.emacs.d/modules/ze-clojure.el
+++ ze-emacs/.emacs.d/modules/ze-clojure.el
@@ -11,40 +11,57 @@
 
   (eval-after-load 'clojure-mode
     '(progn
-       (add-hook 'clojure-mode-hook 'cider-mode)))
+       ;; triger cider-mode when entering clojure-mode
+       (add-hook 'clojure-mode-hook 'cider-mode)
+
+       (defun clojure-hyperspec ()
+	 "Function to lookup a given query using clojuredocs.
+         (This functionality mimics the CL hyperspec lookup functionality in Emacs)"
+	 (interactive)
+	 (ze:browser-jump
+	  (lambda (search-term)
+	    (concat "https://clojuredocs.org/search?q=" search-term))))))
 
   (defun cider-mode-defaults ()
+    (message "Cider mode defaults loading...")
     (setq cider-popup-stacktraces nil)
     (setq cider-show-error-buffer 'only-in-repl)
-    (setq cider-cljs-lein-repl
-	  "(do (require 'figwheel-sidecar.repl-api)
-               (figwheel-sidecar.repl-api/start-figwheel!)
-               (figwheel-sidecar.repl-api/cljs-repl))")
 
-    (dolist (bind '(;;Documentation
-		    ("H-h f" . cider-doc)
-		    ("H-h s" . cider-doc)
-		    ("H-h a" . cider-apropos)
-		    ;;Can we get hyperspec lookup..??
+    ;; TODO: will trigger even in the event of cider using 'boot'.
+    ;; (setq cider-cljs-lein-repl
+    ;; 	  "(do (require 'figwheel-sidecar.repl-api)
+    ;;            (figwheel-sidecar.repl-api/start-figwheel!)
+    ;;            (figwheel-sidecar.repl-api/cljs-repl))")
 
-		    ;; REPL - amalgation of categories
-		    ("H-r r" . cider-jack-in)
-		    ("H-r R" . cider-jack-in-clojurescript)
-		    ("H-r k" . cider-load-file)
-		    ("H-r e" . cider-eval-last-sexp)
-		    ("H-r s" . cider-refresh)
+    (ze:bind cider-mode-map
+	     '(;;Documentation - duplicate binds to mimic CL setup
+	       ("H-h f" . cider-doc)
+	       ("H-h s" . cider-doc)
+	       ("H-h a" . cider-apropos)
+	       ("H-h h" . clojure-hyperspec)
 
+	       ;;REPL - amalgation of categories
 
-		    ("H-r d" . cider-eval-defun-at-point)
-		    ("H-r t" . cider-toggle-trace-var)
-		    ("H-r T" . cider-toggle-trace-ns);; not same as slime(untrace) but useful
+	       ;; load into lein/boot clojure/clojurescript project
+	       ("H-r r" . cider-jack-in)
+	       ("H-r R" . cider-jack-in-clojurescript)
 
+	       ;; load file/sexp into process
+	       ("H-r k" . cider-load-file)
+	       ("H-r e" . cider-eval-last-sexp)
+	       ;; eval the current top-level form
+	       ("H-r d" . cider-eval-defun-at-point)
+	       ;; sync (saved) file changes into running process
+	       ("H-r s" . cider-refresh)
 
-		    ("H-r p" . cider-repl-set-ns) ;; - set REPL NS
+	       ("H-r t" . cider-toggle-trace-var)
+	       ("H-r T" . cider-toggle-trace-ns)
 
+	       ("H-r p" . cider-repl-set-ns) ;; - set REPL NS
 
-		    ("H-r m" . cider-macroexpand-1)
-		    ("H-r M" . cider-macroexpand-all)))))
+	       ;;macro-expansion tools
+	       ("H-r m" . cider-macroexpand-1)
+	       ("H-r M" . cider-macroexpand-all))))
   (add-hook 'cider-mode-hook 'cider-mode-defaults))
 
 (provide 'ze-clojure)
