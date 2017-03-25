da73aeaea95b826c6ea1c5c92159a5a6438dc5b3
Jesper Wendel Devantier <j@pseudonymous.me>
2017-03-25 22:41:33 +0100

    emacs: helm/projectile integration

--- ze-emacs/.emacs.d/modules/ze-projectile.el
+++ ze-emacs/.emacs.d/modules/ze-projectile.el
@@ -5,10 +5,17 @@
   (setq projectile-cache-file (expand-file-name  ".projectile.cache" "~"))
   (projectile-global-mode +1)
 
+  (add-hook 'projectile-mode-hook
+	    (lambda ()
+	      (helm-projectile-on)))
+  (add-hook 'helm--minor-mode-hook
+	    'helm-projectile-on)
+
   (->>
    '(("H-n g" . helm-do-grep-ag)
      ("H-n f" . helm-projectile-find-file)
-     ("H-n c" . helm-M-x))
+     ("H-n c" . helm-M-x)
+     ("C-x b" . helm-mini))
    (ze:bind projectile-mode-map)))
 
 (provide 'ze-projectile)
